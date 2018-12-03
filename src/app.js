d5ecc3308a3a2e4190d18dcbd4b85ede7c4d5de9
Katrina <7407348+katrinasilver@users.noreply.github.com>
2018-12-03 14:24:36 -0800

    removed protected/id routes
    removed protected/id routes

--- src/app.js
+++ src/app.js
@@ -9,26 +9,11 @@ app.use(bodyParser.json())
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 if(process.env.NODE_ENV !== 'production'){ require('dotenv').load() }
 
-
 // Routes
 app.use('/', require('./routes/auth'))
 app.use('/', require('./routes/users'))
 app.use('/invoices', require('./routes/invoices'));
 app.use('/invoices', require('./routes/line_items'));
-
-//authorization & authentication
-const authController = require('./controllers/auth')
-
-app.get('/protected',
-        authController.isAuthenticated,
-        function(req, res, next){ res.send({ id: req.claim.id, message: "For authenticated eyes only" }) })
-
-app.get('/protected/:userId',
-        authController.isAuthenticated,
-        authController.isSelf,
-        function(req, res, next){ res.send({ id: req.claim.id, message: "For your eyes only"}) })
-
-
 
 // Default Route
 app.use(function(req, res, next){
@@ -54,7 +39,6 @@ app.use(function(err, req, res, next){
 const port = process.env.PORT || 3000
 
 app.listen(port, function(){
-
   console.log(`Invoice Creator listening on port ${port}`)
 })
 
