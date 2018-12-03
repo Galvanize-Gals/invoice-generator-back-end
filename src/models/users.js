e8f34675df9e7c37a95312dc5750b5285262bf33
Katrina Agustin <katrina.agustin@gmail.com>
2018-12-03 13:26:31 -0800

    updated function name that checks if email is unique

--- src/models/users.js
+++ src/models/users.js
@@ -15,7 +15,7 @@ function getUser(email){
 
 function create(email, password, first_name, last_name){
 
-  return getOneByUserName(email)
+  return getUser(email)
   .then(function(data){
     if(data) throw { status: 400, message:'User already exists'}
 
