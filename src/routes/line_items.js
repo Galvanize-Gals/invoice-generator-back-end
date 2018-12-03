82c8f9a70bdc932419f7dd3db2108222744e094f
Jamie Kinkead <jkinkead@gmail.com>
2018-12-03 15:30:06 -0800

    line_items create route

--- src/routes/line_items.js
+++ src/routes/line_items.js
@@ -6,7 +6,7 @@ const ctr = require('../controllers/line_items')
 
 router.get('/:invoiceId/line_items', ctr.getAll)
 router.get('/:invoiceId/line_items/:line_item_id', ctr.getOne)
-// router.post('/', ctr.create)
+router.post('/:invoiceId/line_items', ctr.create)
 // router.delete('/:line_item_id', ctr.deleteOne)
 // router.put('/:line_item_id', ctr.edit)
 
