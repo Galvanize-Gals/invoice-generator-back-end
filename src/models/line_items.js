b54055ecfd500b1ae754bf8f3ab3803d371b5d39
Jamie Kinkead <jkinkead@gmail.com>
2018-12-04 09:36:47 -0800

    delete line-items

--- src/models/line_items.js
+++ src/models/line_items.js
@@ -1,18 +1,25 @@
 const knex = require('../../db/index')
 
-function getAll(invoiceId){
-    return knex('line_items').where({ 'line_items.invoice_id':invoiceId })
+function getAll(invoiceId) {
+    return knex('line_items').where({ 'line_items.invoice_id': invoiceId })
 }
 
-function getOne(line_item_id){
+function getOne(line_item_id) {
     return knex('line_items')
-    .where({ 'line_items.id': line_item_id })
+        .where({ 'line_items.id': line_item_id })
 }
 
 function create(desc, quant, rate, inv_id) {
     return knex('line_items')
-    .insert({description:desc, quantity: quant, rate: rate, invoice_id: inv_id})
-    .returning('*')
+        .insert({ description: desc, quantity: quant, rate: rate, invoice_id: inv_id })
+        .returning('*')
+}
+
+function remove(id) {
+    return knex('line_items')
+        .del()
+        .where({ 'line_items.id': id })
+        .returning('*')
 }
 
 
@@ -20,5 +27,6 @@ function create(desc, quant, rate, inv_id) {
 module.exports = {
     getAll,
     getOne,
-    create
+    create,
+    remove
 }
--- src/routes/line_items.js
+++ src/routes/line_items.js
@@ -7,7 +7,7 @@ const ctr = require('../controllers/line_items')
 router.get('/:invoiceId/line_items', ctr.getAll)
 router.get('/:invoiceId/line_items/:line_item_id', ctr.getOne)
 router.post('/:invoiceId/line_items', ctr.create)
-// router.delete('/:line_item_id', ctr.deleteOne)
+router.delete('/:invoiceId/line_items/:line_item_id', ctr.remove)
 // router.put('/:line_item_id', ctr.edit)
 
 
