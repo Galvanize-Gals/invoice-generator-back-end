aa491dbfaf42cdcfa8c01515f04b13c2e4858b62
Jamie Kinkead <jkinkead@gmail.com>
2018-12-04 11:38:39 -0800

    vendor route auth

--- src/routes/vendor.js
+++ src/routes/vendor.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const auth = require('/auth.js')
 
 
 const router = express.Router();
 const ctr = require('../controllers/vendor')
 
-router.get('/:vendorId', ctr.getAll)
+router.get('/:vendorId', auth.authenticated, auth.isSelf, ctr.getAll)
 
 module.exports = router;
