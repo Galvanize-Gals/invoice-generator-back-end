fc88a82da48fef7c401e1de9b1416188e376621a
Jamie Kinkead <jkinkead@gmail.com>
2018-12-04 13:37:26 -0800

    added auth to routes

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -7,9 +7,9 @@ const auth = require('../controllers/auth')
 router.get('/vendor', auth.authenticated, ctr.getAllVendorInvoices)
 router.get('/client', auth.authenticated, ctr.getAllClientInvoices)
 
-router.get('/:invoiceId', ctr.getOne)
-router.post('/', ctr.create)
-router.put('/:invoiceId', ctr.update)
-router.delete('/:invoiceId', ctr.remove)
+router.get('/:invoiceId', auth.authenticated, ctr.getOne)
+router.post('/', auth.authenticated, ctr.create)
+router.put('/:invoiceId', auth.authenticated, ctr.update)
+router.delete('/:invoiceId', auth.authenticated, ctr.remove)
 
 module.exports = router;
