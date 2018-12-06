671112614b36186d20d462710b66ca58152583c0
Celia Marshall <celia.m@gmail.com>
2018-12-06 14:19:18 -0800

    bug fix

--- src/models/users.js
+++ src/models/users.js
@@ -22,7 +22,7 @@ function getOne(userId){
 
 function create(email, password, first_name, last_name, company = null){
 
-  return getUser(email)
+  return getUserByEmail(email)
   .then(function(data){
     if(data) throw { status: 400, message:'User already exists'}
 
